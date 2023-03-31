8770cbe20e64a17e8e6a378edd2ab0a12a72ae21
LyubomirH <lubomir.hristov.highschool@svetlina.net>
2023-03-31 18:27:26 +0300

    zad

---
+++ Numbers N...1.cs
@@ -0,0 +1,15 @@
+﻿namespace Numbers_N._1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int numbre = int.Parse(Console.ReadLine());
+            for (int i = numbre; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
