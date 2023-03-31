f1574f428d7aaf199ca385af343f16bb78974de6
LyubomirH <lubomir.hristov.highschool@svetlina.net>
2023-03-31 18:17:42 +0300

    zad

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Numbers_from_1_to_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
